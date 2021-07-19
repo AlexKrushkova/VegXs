369bd1e3b1ea8ad5a575722731b1c87758599fc4
alexkrushkova <alexkrushkova@gmail.com>
2021-07-19 19:49:52 +0200

    router updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,5 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutingModule = RouterModule.forRoot(routes);
+
