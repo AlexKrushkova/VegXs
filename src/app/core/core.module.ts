c52dcdb3a6c4c18469b9ad0a138a63f3595b7208
alexkrushkova <alexkrushkova@gmail.com>
2021-07-21 08:38:48 +0200

    mini-navigation implemented

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
-
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [NavigationComponent],
+  declarations: [
+    NavigationComponent
+    ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports: [
     NavigationComponent
