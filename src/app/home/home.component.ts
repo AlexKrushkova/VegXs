616ecd9c052a9856a748f24fe453d5431e29e177
alexkrushkova <alexkrushkova@gmail.com>
2021-07-21 07:51:32 +0200

    counter implemented - trial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy {
 
-  constructor() { }
+  counter = 0;
 
-  ngOnInit(): void {
+  intervalSubscription: Subscription;
+
+  constructor() { 
+    interval(1000).subscribe(()=> {
+      this.counter++;
+    });
+  }
+
+  ngOnDestroy(): void {
+  this.intervalSubscription.unsubscribe();
   }
 
 }
