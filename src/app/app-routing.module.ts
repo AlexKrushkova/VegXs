e2947b0c07028e029c52fc81f303eba467b17b63
alexkrushkova <alexkrushkova@gmail.com>
2021-08-02 08:36:35 +0200

    guard implemente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { NavigationComponent } from './core/navigation/navigation.component';
+import { AccessGuard } from './guards/access.guard';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -13,6 +14,7 @@ const routes: Routes = [
   },
   {
     path: 'about',
+    canActivate: [AccessGuard],
     component: AboutComponent  
   },
   
