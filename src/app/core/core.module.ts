75c4af11226400cf1cbc1814e73c95e3ef524aad
alexkrushkova <alexkrushkova@gmail.com>
2021-08-03 07:02:16 +0200

    header and footer added in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavigationComponent,
+    HeaderComponent,
+    FooterComponent
     ],
   imports: [
     CommonModule,
     RouterModule,
   ],
   exports: [
-    NavigationComponent
+    NavigationComponent,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
