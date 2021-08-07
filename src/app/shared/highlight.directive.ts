81874cae4a09a19b4d70f8b63fdbcd87d5e9ff7b
alexkrushkova <alexkrushkova@gmail.com>
2021-08-07 08:36:02 +0200

    highlight directive updated

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnChanges {
+  
+  @Input() color = 'green';
 
-  constructor() { }
+  @Input() isHighlighted = false;
 
+  constructor(
+    private render: Renderer2, //Ivy
+    private elementRef: ElementRef) {
+      
+    }
+
+    highlight(){
+      this.render.setStyle(this.elementRef.nativeElement,
+        'backgroundColor',
+        this.isHighlighted ? this.color : 'inherit')
+    };
+
+    ngOnChanges(): void{
+      this.highlight();
+    }
 }
