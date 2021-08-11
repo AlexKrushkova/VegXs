bd0f0264faf8541d106d7e38be5a22f682f8b099
alexkrushkova <alexkrushkova@gmail.com>
2021-08-11 08:15:21 +0200

    login handler functionality updated

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('f') from: NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void{
+      this.from.valueChanges.subscribe(console.log);
+  }
+
   loginHandler(formData){
+    console.log(this.from);
     console.log(formData)
   }
 
